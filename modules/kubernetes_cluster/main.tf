6e0a4ba0e382f869adf2d5374fc6e3deb635931f
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-10-02 23:11:06 +0300

    Fix formatting in Kubernetes cluster module
    Run "terraform fmt" for "kubernetes_cluster" module.

--- modules/kubernetes_cluster/main.tf
+++ modules/kubernetes_cluster/main.tf
@@ -1,9 +1,9 @@
 resource "openstack_containerinfra_cluster_v1" "cluster_1" {
-  name                 = "${var.cluster_name}"
-  cluster_template_id  = "${var.cluster_template_id}"
-  keypair              = "${var.cluster_keypair}"
-  master_flavor        = "${var.cluster_master_flavor}"
-  flavor               = "${var.cluster_flavor}"
-  master_count         = "${var.cluster_master_count}"
-  node_count           = "${var.cluster_node_count}"
+  name                = "${var.cluster_name}"
+  cluster_template_id = "${var.cluster_template_id}"
+  keypair             = "${var.cluster_keypair}"
+  master_flavor       = "${var.cluster_master_flavor}"
+  flavor              = "${var.cluster_flavor}"
+  master_count        = "${var.cluster_master_count}"
+  node_count          = "${var.cluster_node_count}"
 }

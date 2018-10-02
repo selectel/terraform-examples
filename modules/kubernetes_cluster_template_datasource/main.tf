9b0d1d2c131be2acd905736611467b27e6d5ba92
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-10-02 21:39:07 +0300

    Add Container Infra Cluster Template datasource
    Add module for retrieving ID of an existing OpenStack Container Infra
    Cluster Template.

---
+++ modules/kubernetes_cluster_template_datasource/main.tf
@@ -0,0 +1,3 @@
+data "openstack_containerinfra_clustertemplate_v1" "clustertemplate_1" {
+  name = "${var.clustertemplate_name}"
+}
---
+++ modules/kubernetes_cluster_template_datasource/outputs.tf
@@ -0,0 +1,3 @@
+output "cluster_template_id" {
+  value = "${data.openstack_containerinfra_clustertemplate_v1.clustertemplate_1.id}"
+}

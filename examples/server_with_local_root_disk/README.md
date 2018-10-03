2c18058979f566bd26f1cc65b64d39c9c63c812d
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-10-03 17:52:54 +0300

    Examples: add "server_local_root_disk"
    Add example environment of server with local root disk.

---
+++ examples/server_with_local_root_disk/README.md
@@ -0,0 +1,16 @@
+# Server with local root disk
+
+This environment will create following resources in the OpenStack project:
+
+ * keypair
+ * flavor
+ * server with associated floating IP and local root disk
+
+To run this environment you need to export authorization variables that can be
+obtained from [VPC access page](https://my.selectel.ru/vpc/access):
+
+```bash
+$ source rc.sh
+$ terraform init
+$ terraform apply 
+```
---
+++ examples/server_with_local_root_disk/main.tf
@@ -0,0 +1,56 @@
+# Generate random string that will be used as name for all resources.
+resource "random_string" "random_name" {
+  length  = 12
+
+  special = false
+  upper   = false
+  number  = false
+}
+
+# Create OpenStack keypair with current public key.
+module "keypair" {
+  source             = "../../modules/keypair"
+
+  keypair_name       = "${random_string.random_name.result}-keypair"
+  keypair_public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+# Create OpenStack flavor without local disk.
+module "flavor" {
+  source               = "../../modules/flavor"
+
+  flavor_name          = "${random_string.random_name.result}-flavor"
+  flavor_vcpus         = 4
+  flavor_ram_mb        = 4096
+  flavor_local_disk_gb = 8
+}
+
+# Search for the public image ID.
+module "image_datasource" {
+  source     = "../../modules/image_datasource"
+
+  image_name = "Ubuntu 16.04 LTS 64-bit"
+}
+
+# Create OpenStack Compute instance.
+module "server_local_root_disk" {
+  source                        = "../../modules/server_local_root_disk"
+
+  server_name                   = "${random_string.random_name.result}-server"
+
+  server_flavor_id              = "${module.flavor.flavor_id}"
+  server_keypair_name           = "${module.keypair.keypair_id}"
+  server_image_id               = "${module.image_datasource.image_id}"
+  server_availability_zone      = "ru-3a"
+}
+
+# Create single OpenStack Floating IP for the instance.
+module "floatingip" {
+  source = "../../modules/floatingip"
+}
+
+# Associate Floating IP with instance network interface.
+resource "openstack_networking_floatingip_associate_v2" "association_1" {
+  port_id     = "${module.server_local_root_disk.server_port_id}"
+  floating_ip = "${module.floatingip.floatingip_address}"
+}

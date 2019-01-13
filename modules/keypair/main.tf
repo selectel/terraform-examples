0632cb94dfeb74b134de015281d1704cc2aec9bf
Andrei Ozerov <andrei.ozerov92@gmail.com>
2019-01-13 22:45:00 +0300

    Keypair: use SelVPC keypair

--- modules/keypair/main.tf
+++ modules/keypair/main.tf
@@ -1,4 +1,5 @@
-resource "openstack_compute_keypair_v2" "keypair_1" {
+resource "selvpc_resell_keypair_v2" "keypair_1" {
   name       = "${var.keypair_name}"
   public_key = "${var.keypair_public_key}"
+  user_id    = "${var.keypair_user_id}"
 }
--- modules/keypair/vars.tf
+++ modules/keypair/vars.tf
@@ -1,8 +1,12 @@
 variable "keypair_name" {
-  description = "Name of the OpenStack Compute Keypair"
+  description = "Name of the SelVPC V2 Keypair"
   default     = "keypair_1"
 }
 
 variable "keypair_public_key" {
-  description = "Public key for the OpenStack Compute Keypair"
+  description = "Public key for the SelVPC V2 Keypair"
+}
+
+variable "keypair_user_id" {
+  description = "Owner of the SelVPC V2 Keypair"
 }

dc9e9e5636b48822f2ba7a0192033ab36cf8e7c4
Andrei Ozerov <andrei.ozerov92@gmail.com>
2019-01-13 22:45:14 +0300

    Modules: add SelVPC user
    Add new module.

---
+++ modules/user/outputs.tf
@@ -0,0 +1,3 @@
+output "user_id" {
+  value = "${selvpc_resell_user_v2.user_1.id}"
+}
---
+++ modules/user/vars.tf
@@ -0,0 +1,8 @@
+variable "user_name" {
+  description = "Name of the SelVPC V2 User"
+  default     = "user_1"
+}
+
+variable "user_password" {
+  description = "Password of the SelVPC V2 User"
+}

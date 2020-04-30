c63197da1e1a1328061ae85480d3ecce58d8d9f9
Sergei Kolyshkin <kolyshkin@selectel.ru>
2020-04-30 15:56:57 +0300

    Add mks cluster module

---
+++ modules/mks/cluster/outputs.tf
@@ -0,0 +1,11 @@
+output "cluster_id" {
+  value = selectel_mks_cluster_v1.cluster_1.id
+}
+
+output "project_id" {
+  value = selectel_mks_cluster_v1.cluster_1.project_id
+}
+
+output "region" {
+  value = selectel_mks_cluster_v1.cluster_1.region
+}

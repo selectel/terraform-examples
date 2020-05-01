f3d4f43b5bfa8c4796db02d8c595b4389b74bf64
Nikita Kunets <nik.kunets@gmail.com>
2020-05-01 09:34:29 +0300

    Add outputs to examples

---
+++ examples/vpc/server_local_and_remote_disks/output.tf
@@ -0,0 +1,7 @@
+output "server_id" {
+  value = module.server_local_and_remote_disks.server_id
+}
+
+output "floating_ip" {
+  value = module.server_local_and_remote_disks.floating_ip
+}
---
+++ examples/vpc/server_remote_root_disk_two_ports/output.tf
@@ -0,0 +1,7 @@
+output "server_id" {
+  value = module.server_remote_root_disk_two_ports.server_id
+}
+
+output "floating_ip" {
+  value = module.server_remote_root_disk_two_ports.floating_ip
+}

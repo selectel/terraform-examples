4268881c8ae15e2ee0cc764afbfa96816cc66f28
Konstantin Eremin <kod-t@mail.ru>
2021-01-18 22:37:51 +0300

    Added Openstack provider source location (#20)

--- examples/vpc/several_servers_one_network/versions.tf
+++ examples/vpc/several_servers_one_network/versions.tf
@@ -3,6 +3,9 @@ terraform {
     selectel = {
       source = "selectel/selectel"
     }
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+    }
   }
   required_version = ">= 0.13"
 }
